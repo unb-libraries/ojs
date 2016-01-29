b3d0c2b52ffefc3c288443f20d11fa85a521c271
Brian Cassidy <brian.cassidy@gmail.com>
2016-01-29 08:45:59 -0400

    quote strings for bash comparisons

--- etcscripts/crkn/ipSubscriptions/check-crkn.sh
+++ etcscripts/crkn/ipSubscriptions/check-crkn.sh
@@ -6,7 +6,7 @@ SUBJECT='CRKN IP Subscription Data'
 TOPIC='arn:aws:sns:us-east-1:344420214229:unb_lib_crkn_subscriber_data'
 
 RECENT=`find $LOG -mtime -1`
-if [ $RECENT == '' ]
+if [ "$RECENT" == '' ]
 then
   OUT=`$EC2BIN -t $TOPIC -s "$SUBJECT" -m 'FAIL: Log file not updated'`
   exit
@@ -14,7 +14,7 @@ fi
 
 COUNT=`grep Entries $LOG|awk '{print $NF}'`
 
-if [ $COUNT == '' ] || [ $COUNT == 0 ]
+if [ "$COUNT" == '' ] || [ "$COUNT" == "0" ]
 then
   OUT=`$EC2BIN -t $TOPIC -s "$SUBJECT" -m 'FAIL: No entries synchronized'`
 else
